08d5df792b1b6f9ca83a29c2981a156e86781c3b
BFBacchi <bfbacchi@gmail.com>
2023-05-08 03:12:09 -0300

    se agrega package controller y se deja vacio por el momento

---
+++ src/main/java/com/PortfolioBacchi/backend/Security/Controller/AuthController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.PortfolioBacchi.backend.Security.Controller;
+
+/**
+ *
+ * @author W10-PC
+ */
+public class AuthController {
+    
+}
